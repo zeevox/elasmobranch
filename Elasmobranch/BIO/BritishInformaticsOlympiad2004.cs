b900c0d22f145680ebed9a0e5f8678ed732b91c3
Timothy Langer <zeevox.dev@gmail.com>
2020-09-23 15:07:23 +0100

    BIO 2004 Question 1a
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ Elasmobranch/BIO/BritishInformaticsOlympiad2004.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Elasmobranch.BIO
+{
+    public class BritishInformaticsOlympiad2004
+    {
+        public static DateTime MayanToGregorian(int baktuns, int katuns, int tuns, int uinals, int kins)
+        {
+            baktuns -= 13;
+            katuns -= 20;
+            tuns -= 7;
+            uinals -= 16;
+            kins -= 3;
+            
+            return new DateTime(2000, 1, 1)
+                .AddDays(baktuns * 20 * 20 * 18 * 20 + 
+                         katuns       * 20 * 18 * 20 + 
+                         tuns              * 18 * 20 + 
+                         uinals                 * 20 +
+                         kins                         );
+        }
+    }
+}
