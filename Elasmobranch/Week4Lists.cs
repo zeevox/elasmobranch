acee66f8c35fbeb5b4f06ca5e962bd769608105d
Timothy Langer <zeevox.dev@gmail.com>
2020-10-06 12:14:24 +0100

    Add a bit of Week 4 code
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ Elasmobranch/Week4Lists.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Elasmobranch
+{
+    public class Week4Lists<T> : List<T>
+    {
+        /// <summary>
+        /// Get all the duplicated elements in this list
+        /// </summary>
+        /// <returns>A list of the duplicated elements in this list</returns>
+        public List<T> GetDuplicates()
+        {
+            return this.GroupBy(t => t).SelectMany(group => group.Skip(1)).ToList();
+        }
+
+        /// <summary>
+        /// Return whether the provided list is a subset of this one
+        /// </summary>
+        /// <param name="subset">A second list which is potentially a subset of this one</param>
+        /// <returns>Whether the provided list is a subset of this one</returns>
+        public bool IsSubset(IEnumerable<T> subset)
+        {
+            // Except removes all elements from the provided "subset" list that are present in this list
+            // Any then returns true if there are any elements remaining
+            // If there are elements remaining then they must be elements that are inside the "subset" but not in list
+            // Then negate the boolean for the purposes of this function
+            return !subset.Except(this).Any();
+        }
+        
+        // for an implementation of a QuickSort algorithm see Week3Sorting.cs
+    }
+}
