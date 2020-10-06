3d47856a5292ea4ce682cc3368dbc0677ebc3136
Timothy Langer <zeevox.dev@gmail.com>
2020-10-06 21:39:19 +0100

    Update Caesar cipher, add Vigenère cipher
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- Elasmobranch/Week5Encryption.cs
+++ Elasmobranch/Week5Encryption.cs
@@ -6,37 +6,124 @@ namespace Elasmobranch
 {
     public static class Week5Encryption
     {
-        private const string AsciiLowercase = "abcdefghijklmnopqrstuvwxyz";
-        private static readonly string AsciiUppercase = AsciiLowercase.ToUpper();
-
-        public static string CaesarCipher(string plaintext, int rot, bool squash = false, bool decrypt = false)
+        /// <summary>
+        /// Functions relating to the Caesar Cipher
+        /// </summary>
+        public static class CaesarCipher
         {
-            if (decrypt) rot = AsciiLowercase.Length - rot;
+            private const string AsciiLowercase = "abcdefghijklmnopqrstuvwxyz";
+            private static readonly string AsciiUppercase = AsciiLowercase.ToUpper();
 
-            var lowerCaseShifted = AsciiLowercase.Substring(rot) + AsciiLowercase.Substring(0, rot);
-            var upperCaseShifted = AsciiUppercase.Substring(rot) + AsciiUppercase.Substring(0, rot);
-
-            var encoded = new StringBuilder();
-            foreach (var character in plaintext)
+            /// <summary>
+            /// Apply the Caesar cipher to a string of text
+            /// </summary>
+            /// <param name="input">The input text</param>
+            /// <param name="rot">The rotation of the Caesar Cipher</param>
+            /// <param name="squash">Whether to leave punctuation included</param>
+            /// <param name="decrypt">Whether to run it in reverse, i.e. decipher instead of cipher</param>
+            /// <returns>The ciphered text</returns>
+            private static string RunCipher(string input, int rot, bool squash = false, bool decrypt = false)
             {
-                if (!squash && !char.IsLetter(character))
+                if (decrypt) rot = AsciiLowercase.Length - rot;
+
+                var encoded = new StringBuilder();
+                foreach (var character in input.Where(character => !squash || char.IsLetter(character)))
                 {
-                    encoded.Append(character);
+                    encoded.Append(EncipherSingleChar(character, rot));
                 }
-                else
-                {
-                    if (char.IsUpper(character))
-                    {
-                        encoded.Append(upperCaseShifted[character - 65]);
-                    }
-                    else if (char.IsLower(character))
-                    {
-                        encoded.Append(lowerCaseShifted[character - 97]);
-                    }
-                }
+
+                return encoded.ToString();
             }
 
-            return encoded.ToString();
+            /// <summary>
+            /// Apply the Caesar cipher to a string of text
+            /// </summary>
+            /// <param name="plaintext">The input text</param>
+            /// <param name="rot">The rotation of the Caesar Cipher</param>
+            /// <param name="squash">Whether to leave punctuation included</param>
+            /// <returns>The ciphered text</returns>
+            public static string Encipher(string plaintext, int rot, bool squash = false)
+            {
+                return RunCipher(plaintext, rot, squash);
+            }
+
+            /// <summary>
+            /// Decipher a Caesar ciphered string of text
+            /// </summary>
+            /// <param name="ciphered">The input ciphered text</param>
+            /// <param name="rot">The rotation of the Caesar Cipher</param>
+            /// <param name="squash">Whether to leave punctuation included</param>
+            /// <returns>The deciphered text</returns>
+            public static string Decipher(string ciphered, int rot, bool squash = false)
+            {
+                return RunCipher(ciphered, rot, squash, true);
+            }
+
+            /// <summary>
+            /// Caesar Cipher shift a single character
+            /// </summary>
+            /// <param name="character">Input character</param>
+            /// <param name="rot">Number of places to shift by</param>
+            /// <returns>Ciphered character</returns>
+            public static char EncipherSingleChar(char character, int rot)
+            {
+                var lowerCaseShifted = AsciiLowercase.Substring(rot) + AsciiLowercase.Substring(0, rot);
+                var upperCaseShifted = AsciiUppercase.Substring(rot) + AsciiUppercase.Substring(0, rot);
+
+                if (char.IsLetter(character))
+                {
+                    return char.IsUpper(character)
+                        ? upperCaseShifted[character - 65]
+                        : lowerCaseShifted[character - 97];
+                }
+
+                return character;
+            }
+        }
+
+        /// <summary>
+        /// Functions relating to the Vigenère Cipher
+        /// </summary>
+        public static class VigenereCipher
+        {
+            /// <summary>
+            /// Encipher a string of plaintext using the Vigenère cipher
+            /// </summary>
+            /// <param name="plaintext">The text to be ciphered</param>
+            /// <param name="keyword">The key to use for the Vigenère cipher</param>
+            /// <param name="squash">Whether to strip non-alphabet characters from the output</param>
+            /// <returns>The input enciphered with the Vigenère cipher</returns>
+            public static string Encipher(string plaintext, string keyword, bool squash = true)
+            {
+                // convert the key into a char array, chars can also be used as numbers
+                // uppercase so that they are homogeneously same case so we do not have to consider cases (lower/upper)
+                var keywordShifts = keyword.ToUpper().ToCharArray();
+
+                // Strip punctuation at the very start (by default)
+                if (squash) plaintext = new string(plaintext.Where(char.IsLetter).ToArray());
+
+                var encoded = new StringBuilder();
+                
+                // count the number of characters that we did not cipher due to them being non-alphabet chars
+                // this is necessary to prevent the key going off-place if we are leaving punctuation in the output
+                var squashCount = 0;
+                
+                for (var i = 0; i < plaintext.Length; i++)
+                {
+                    if (!char.IsLetter(plaintext[i]))
+                    {
+                        squashCount += 1;
+                        encoded.Append(plaintext[i]);
+                    }
+                    else
+                    {
+                        encoded.Append(CaesarCipher.EncipherSingleChar(plaintext[i], 
+                            keywordShifts[(i - squashCount) % keywordShifts.Length] - 65));
+                    }
+                }
+
+                return encoded.ToString();
+            }
         }
     }
 }
--- Tests/Week5Encryption.cs
+++ Tests/Week5Encryption.cs
@@ -16,19 +16,39 @@ namespace Tests
         [TestCase("Wkhuhvdvwdupdqzdlwlqjlqwkhvnb", "There's-a-starman-waiting-in-the-sky", 3, true)]
         [TestCase("okffng-Qwvb", "middle-Outz", 2)]
         [TestCase("okffngQwvb", "middle-Outz", 2, true)]
-        // Decryption
-        [TestCase("abcd","bcde",  1, false, true)]
-        [TestCase("abcd-efgh","cdef-ghij",  2, false, true)]
-        [TestCase("abcdefgh","cdefghij",  2, true, true)]
-        [TestCase("howdy", "krzgb", 3, false, true)]
-        [TestCase("abcdabcdddeebcadd","abcdabcdddeebcadd",  26, false, true)]
-        [TestCase("There's-a-starman-waiting-in-the-sky","Wkhuh'v-d-vwdupdq-zdlwlqj-lq-wkh-vnb",  3, false, true)]
-        [TestCase("Theresastarmanwaitinginthesky", "Wkhuhvdvwdupdqzdlwlqjlqwkhvnb", 3, true, true)]
-        [TestCase("middle-Outz", "okffng-Qwvb", 2, false, true)]
-        [TestCase("middleOutz","okffngQwvb",  2, true, true)]
-        public void CaesarCipherTest(string expected, string plaintext, int rot, bool squash = false, bool decrypt = false)
+        public void CaesarCipherTest(string expected, string plaintext, int rot, bool squash = false)
         {
-            Assert.AreEqual(expected, CaesarCipher(plaintext, rot, squash, decrypt));
+            Assert.AreEqual(expected, CaesarCipher.Encipher(plaintext, rot, squash));
+        }
+
+        [Test]
+        // Decryption
+        [TestCase("abcd","bcde",  1, false)]
+        [TestCase("abcd-efgh","cdef-ghij",  2, false)]
+        [TestCase("abcdefgh","cdefghij",  2, true)]
+        [TestCase("howdy", "krzgb", 3, false)]
+        [TestCase("abcdabcdddeebcadd","abcdabcdddeebcadd",  26, false)]
+        [TestCase("There's-a-starman-waiting-in-the-sky","Wkhuh'v-d-vwdupdq-zdlwlqj-lq-wkh-vnb",  3, false)]
+        [TestCase("Theresastarmanwaitinginthesky", "Wkhuhvdvwdupdqzdlwlqjlqwkhvnb", 3, true)]
+        [TestCase("middle-Outz", "okffng-Qwvb", 2, false)]
+        [TestCase("middleOutz","okffngQwvb",  2, true)]
+        public void CaesarDecipherTest(string expected, string ciphered, int rot, bool squash = false)
+        {
+            Assert.AreEqual(expected, CaesarCipher.Decipher(ciphered, rot, squash));
+        }
+
+        [Test]
+        [TestCase("alpvsuhypn", "helloworld", "thekey")]
+        [TestCase("ashyspvftgocsw", "he's gone bananas", "topsecret")]
+        [TestCase("Vyc fnqkm spdpv nqo hjfxa qmcg mpqtkctg tibp bdza.", "The quick brown fox jumps over thirteen lazy dogs.", "cryptii", false)]
+        [TestCase("Vycfnqkmspdpvnqohjfxaqmcgmpqtkctgtibpbdza", "The quick brown fox jumps over thirteen lazy dogs.", "cryptii")]
+        [TestCase("Bggafyqrh xs mzg srpar xora sg ezuooqso, xlqfz qwo'x ezm ric b fiq gcwymh fq owti us jxm.", "According to all known laws of aviation, there isn't any way a bee should be able to fly.", "beemovie", false)]
+        [TestCase("Bggafyqrhxsmzgsrparxorasgezuooqsoxlqfzqwoxezmricbfiqgcwymhfqowtiusjxm", "According to all known laws of aviation, there isn't any way a bee should be able to fly.", "beemovie")]
+        [TestCase("\"Vx's ezr mfrm\" – jr grzpb!", "\"It's not true\" – he cried!", "nEaRlYtOXiC", false)]
+        [TestCase("Vxsezrmfrmjrgrzpb", "\"It's not true\" – he cried!", "nEaRlYtOXiC")]
+        public void VigenereCipherTest(string expected, string plaintext, string keyword, bool squash = true)
+        {
+            Assert.AreEqual(expected, VigenereCipher.Encipher(plaintext, keyword, squash));
         }
     }
 }
