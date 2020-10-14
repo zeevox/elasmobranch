08cb98706aa7098b25e6496ac8ae6984bbab5a9b
Timothy Langer <zeevox.dev@gmail.com>
2020-10-14 11:04:47 +0100

    Add Week 6 exceptions code
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ Elasmobranch/Week6Exceptions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using Syroot.Windows.IO;
+
+namespace Elasmobranch
+{
+    public class Week6Exceptions
+    {
+        /// <summary>
+        /// Get the filename from the given URL
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        private static string Basename(Uri uri)
+        {
+            return uri.LocalPath;
+        }
+
+        /// <summary>
+        /// Download a file from the internet given a URL
+        /// </summary>
+        /// <param name="url">The web file URL</param>
+        public static bool DownloadFile(string url)
+        {
+            var uri = new Uri(url);
+
+            try
+            {
+                using var client = new WebClient();
+                client.DownloadFile(uri, new KnownFolder(KnownFolderType.Downloads).Path + Basename(uri));
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.GetBaseException().Message);
+                
+                if (e.Status == WebExceptionStatus.ProtocolError)
+                {
+                    Console.WriteLine($"Status code: {(int) e.Status}");
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/Week6Exceptions.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using static Elasmobranch.Week6Exceptions;
+
+namespace Tests
+{
+    public class Week6Exceptions
+    {
+        [Test]
+        [TestCase("https://zeevox.net/images/avatar_122px.webp")]
+        [TestCase("http://zeevox.net/images/avatar_122px.webp")]
+        [TestCase("hsts://zeevox.net/images/avatar_122px.webp")]
+        [TestCase("http://example.org/images/avatar_122px.webp")]
+        [TestCase("http://abysmalgreenparrot.com/images/parrot.jpg")]
+        public void FileDownloadTest(string url)
+        {
+            DownloadFile(url);
+        }
+    }
+}
