a7a8f0e3bbe22b0bbd72374fa659f38fac5d89ec
Timothy Langer <zeevox.dev@gmail.com>
2020-10-15 15:25:38 +0100

    Timed coding task 2020-10-15
    * AQA Specimen Paper 7517 Section B (20 mins)
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ Elasmobranch/Week6TimedCodingTask.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Elasmobranch
+{
+    /// <summary>
+    ///     20 minutes to code an answer to Section B of AQA 7517 Specimen Paper
+    ///     Available for viewing at https://store.aqa.org.uk/resources/computing/AQA-75171-SQP.PDF
+    /// </summary>
+    public class Week6TimedCodingTask
+    {
+        public static string ConvertDecimalToBinaryReversed(int dec)
+        {
+            var remainders = new StringBuilder();
+            do
+            {
+                remainders.Append((dec % 2).ToString());
+                dec = (dec - dec % 2) / 2;
+            } while (dec > 0);
+
+            return remainders.ToString();
+        }
+
+        public static string ConvertDecimalToBinary(int dec)
+        {
+            var reversed = ConvertDecimalToBinaryReversed(dec);
+            var result = new StringBuilder();
+            for (var i = reversed.Length - 1; i >= 0; i--) result.Append(reversed[i]);
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Tests/Week6TimedCodingTaskTest.cs
@@ -0,0 +1,21 @@
+﻿using Elasmobranch;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Week6TimedCodingTaskTest
+    {
+        [TestCase(210, "01001011", "11010010")]
+        public void DecimalToBinaryTest(int dec, string expectedReversed, string expected)
+        {
+            var actualReversed = Week6TimedCodingTask.ConvertDecimalToBinaryReversed(dec);
+            Assert.AreEqual(expectedReversed, actualReversed);
+
+            var actual = Week6TimedCodingTask.ConvertDecimalToBinary(dec);
+            Assert.AreEqual(expected, actual);
+
+            TestContext.WriteLine(
+                $"Decimal input: {dec} -> Reversed binary: {actualReversed} -> Binary output: {actual}");
+        }
+    }
+}
