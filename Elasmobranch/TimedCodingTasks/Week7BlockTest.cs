09f2cd0cec14a5928036d93e26d5552d6b1d954a
Timothy Langer <zeevox.dev@gmail.com>
2020-11-02 10:08:21 +0000

    Add November block test solutions
    * Three coding tasks were completed in test conditions on Monday 2nd November
    * No online resources were permitted, only a single physical A4 cheatsheet
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ Elasmobranch/TimedCodingTasks/Week7BlockTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Elasmobranch.TimedCodingTasks
+{
+    public class Week7BlockTest
+    {
+        /// <summary>
+        ///     Count the number of occurrences of an integer in a given list of integers
+        /// </summary>
+        /// <param name="source">List of integers</param>
+        /// <param name="entry">Integer to search for</param>
+        /// <returns>Number of occurrences of entry in source</returns>
+        private static int CountOccurrences(List<int> source, int entry)
+        {
+            var count = 0;
+            foreach (var t in source)
+                if (t == entry)
+                    count++;
+
+            return count;
+        }
+
+        /// <summary>
+        ///     Given a list of numbers in which two numbers occur once and the rest occur only twice,
+        ///     return the sum of the numbers that occur only once.
+        /// </summary>
+        /// <param name="source">List of numbers</param>
+        /// <returns>Sum of the numbers that occur only once in the list</returns>
+        public static int Repeats(List<int> source)
+        {
+            var singularEntries = source.Where(entry => CountOccurrences(source, entry) == 1).ToList();
+
+            return singularEntries[0] + singularEntries[1];
+        }
+
+        /// <summary>
+        ///     Given a string of words return the length of the shortest words
+        /// </summary>
+        /// <param name="s">string of space-separated words</param>
+        /// <returns>the length of the shortest word</returns>
+        public static int FindShort(string s)
+        {
+            var listOfWords = s.Split(" ").ToList();
+            var counts = listOfWords.Select(word => word.Length).ToList();
+            return listOfWords[counts.IndexOf(counts.Min(i => i))].Length;
+        }
+
+        /// <summary>
+        ///     Given an integer n return a jagged array of length n filled with sub-arrays of
+        ///     increasing length. The sub-arrays should be filled with ones.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int[][] Pyramid(int n)
+        {
+            var topArray = new int[n][];
+            for (var i = 0; i < n; i++)
+            {
+                var subArray = new int[i+1];
+                for (var j = 0; j < subArray.Length; j++) subArray[j] = 1;
+                
+                topArray[i] = subArray;
+            }
+
+            return topArray;
+        }
+    }
+}
---
+++ Tests/TimedCodingTasks/Week7BlockTestTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using static Elasmobranch.TimedCodingTasks.Week7BlockTest;
+
+namespace Tests.TimedCodingTasks
+{
+    public class Week7BlockTestTests
+    {
+        [Test]
+        public void RepeatsTest()
+        {
+            Assert.AreEqual(15, Repeats(new List<int> {4, 5, 7, 5, 4, 8}));
+            Assert.AreEqual(19, Repeats(new List<int> {9, 10, 19, 13, 19, 13}));
+            Assert.AreEqual(12, Repeats(new List<int> {16, 0, 11, 4, 8, 16, 0, 11}));
+        }
+
+        [Test]
+        public void FindShortsTest()
+        {
+            Assert.AreEqual(3, FindShort("bitcoin take over the world maybe who knows perhaps"));
+            Assert.AreEqual(2, FindShort("consectetur adipiscing elit sed do eiusmod tempor incididiunt"));
+        }
+
+        [Test]
+        public void PyramidTest()
+        {
+            Assert.AreEqual(new int[0], Pyramid(0));
+            Assert.AreEqual(new[]
+            {
+                new[] {1}
+            }, Pyramid(1));
+            Assert.AreEqual(new[]
+            {
+                new[] {1},
+                new[] {1, 1}
+            }, Pyramid(2));
+            Assert.AreEqual(new[]
+            {
+                new[] {1},
+                new[] {1, 1},
+                new[] {1, 1, 1}
+            }, Pyramid(3));
+    }
+    }
+}
